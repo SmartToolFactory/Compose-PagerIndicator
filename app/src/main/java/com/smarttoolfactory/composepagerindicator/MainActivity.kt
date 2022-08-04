56f0cafd4a8089bb938b39b66817e01aee98b0e4
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-08-04 17:13:53 +0300

    create horizontal and vertical orientations

--- app/src/main/java/com/smarttoolfactory/composepagerindicator/MainActivity.kt
+++ app/src/main/java/com/smarttoolfactory/composepagerindicator/MainActivity.kt
@@ -3,23 +3,31 @@ package com.smarttoolfactory.composepagerindicator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.VerticalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.smarttoolfactory.composepagerindicator.ui.theme.ComposePagerIndicatorTheme
+import com.smarttoolfactory.indicator.IndicatorOrientation
 import com.smarttoolfactory.indicator.PagerIndicator
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,31 +37,117 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(top = 50.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-
-                        val pagerState = rememberPagerState(initialPage = 0)
-
-                        Spacer(modifier = Modifier.height(20.dp))
-                        PagerIndicator(pagerState = pagerState)
-
-
-
-                        HorizontalPager(
-                            modifier = Modifier.fillMaxHeight(),
-                            count = 10,
-                            state = pagerState,
-                        ) {
-                            Text("Text $it", fontSize = 60.sp, fontWeight = FontWeight.Bold)
-                        }
-                    }
+                    PagerIndicatorSample()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+private fun PagerIndicatorSample() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(Modifier.height(40.dp))
+        val pagerState1 = rememberPagerState(initialPage = 0)
+        val coroutineScope = rememberCoroutineScope()
+
+        PagerIndicator(pagerState = pagerState1) {
+            coroutineScope.launch {
+                pagerState1.scrollToPage(it)
+            }
+        }
+
+        HorizontalPager(
+            count = 10,
+            state = pagerState1,
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .shadow(1.dp, RoundedCornerShape(8.dp))
+                    .background(Color.White)
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    "Text $it",
+                    fontSize = 40.sp,
+                    color = Color.Gray
+                )
+            }
+        }
+
+        val pagerState2 = rememberPagerState(initialPage = 0)
+
+        PagerIndicator(
+            pagerState = pagerState2,
+            indicatorSize = 24.dp,
+            indicatorCount = 7,
+            activeColor = Color(0xffFFC107),
+            inActiveColor = Color(0xffFFECB3),
+            indicatorShape = CutCornerShape(10.dp)
+        )
+        HorizontalPager(
+            count = 10,
+            state = pagerState2,
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .shadow(1.dp, RoundedCornerShape(8.dp))
+                    .background(Color.White)
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    "Text $it",
+                    fontSize = 40.sp,
+                    color = Color.Gray
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val pagerState3 = rememberPagerState(initialPage = 0)
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            PagerIndicator(
+                pagerState = pagerState3,
+                orientation = IndicatorOrientation.Vertical
+            )
+
+            Spacer(modifier = Modifier.width(20.dp))
+            VerticalPager(
+                count = 10,
+                state = pagerState3,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .shadow(1.dp, RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        "Text $it",
+                        fontSize = 40.sp,
+                        color = Color.Gray
+                    )
+                }
+            }
+        }
+    }
+}
--- indicator/src/main/java/com/smarttoolfactory/indicator/PagerIndicator.kt
+++ indicator/src/main/java/com/smarttoolfactory/indicator/PagerIndicator.kt
@@ -1,16 +1,13 @@
 package com.smarttoolfactory.indicator
 
-import android.view.animation.DecelerateInterpolator
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,43 +18,46 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import com.google.accompanist.pager.VerticalPager
 
 
+/**
+ * Creates indicator  aligned horizontally or verticall based on [orientation]
+ * for [HorizontalPager] or [VerticalPager] with [indicatorCount]
+ * with option to click when [onClick]
+ * is set.
+ *
+ * @param pagerState state of pager
+ * @param indicatorCount counter of visible indicator items
+ * @param indicatorSize size of each indicator item
+ * @param indicatorShape shape of the indicator items, by default it's [CircleShape]
+ * @param space between indicator items
+ * @param activeColor color of indicator when current page points to this item
+ * @param inActiveColor color for indicators that are not active
+ * @param orientation determines whether items should be aligned horizontally or vertically
+ * @param onClick action that should be taken when any indicator items are touched
+ */
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun PagerIndicator(
     modifier: Modifier = Modifier,
     pagerState: PagerState,
-    indicatorCount: Int = 7,
-    indicatorSize: Dp = 20.dp,
+    indicatorCount: Int = 5,
+    indicatorSize: Dp = 16.dp,
     indicatorShape: Shape = CircleShape,
-    space: Dp = 10.dp,
-    activeColor: Color = Color.Red,
+    space: Dp = 8.dp,
+    activeColor: Color = Color(0xffEC407A),
     inActiveColor: Color = Color.LightGray,
-    onClick: (CoroutineScope.(Int) -> Unit)? = null
+    orientation: IndicatorOrientation = IndicatorOrientation.Horizontal,
+    onClick: ((Int) -> Unit)? = null
 ) {
 
     val listState = rememberLazyListState()
 
-
-    var text by remember { mutableStateOf("") }
-
-    Canvas(modifier = Modifier.size(100.dp) ){
-        size
-    }
-
     val totalWidth: Dp = indicatorSize * indicatorCount + space * (indicatorCount - 1)
     val widthInPx = LocalDensity.current.run { indicatorSize.toPx() }
-    val totalWidthInPx = LocalDensity.current.run { totalWidth.toPx() }
-
-    val items by remember {
-        derivedStateOf {
-            listState.layoutInfo.visibleItemsInfo
-        }
-    }
 
     val currentItem by remember {
         derivedStateOf {
@@ -66,123 +66,135 @@ fun PagerIndicator(
     }
 
     val itemCount = pagerState.pageCount
-    val currentPageOffset = pagerState.currentPageOffset
-
-    if (items.isNotEmpty()) {
-        var tempText =
-            "totalWidth: $totalWidthInPx\n" +
-                    "currentPageOffset: $currentPageOffset\n" +
-                    " Item count ${items.size}, currentItem: $currentItem\n"
-
-        items.forEach { lazyListItemInfo ->
-            tempText += "index: ${lazyListItemInfo.index}, " +
-                    "offset: ${lazyListItemInfo.offset}, " +
-                    "size: ${lazyListItemInfo.size}\n"
-        }
-
-        text = tempText
-    }
-
-    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(key1 = currentItem) {
         val viewportSize = listState.layoutInfo.viewportSize
-        listState.animateScrollToItem(
-            currentItem,
-            (widthInPx / 2 - viewportSize.width / 2).toInt()
-        )
-    }
-
-    BoxWithConstraints(Modifier.width(totalWidth)) {
-
-        LazyRow(
-            modifier = Modifier.border(1.dp, Color.Green),
-            contentPadding = PaddingValues(vertical = space),
-
-            state = listState,
-            horizontalArrangement = Arrangement.spacedBy(space),
-        ) {
-
-            items(itemCount) { index ->
-
-                val isSelected = (index == currentItem)
-
-                // Index of item in center when odd number of indicators are set
-                // for 5 indicators this is 2nd indicator place
-                val centerItemIndex = indicatorCount / 2
-
-                val right1 =
-                    (currentItem <= centerItemIndex && itemCount > indicatorCount &&
-                            index == indicatorCount - 1)
-
-                val right2 =
-                    (index >= currentItem + centerItemIndex &&
-                            currentItem > centerItemIndex &&
-                            currentItem < indicatorCount - 1)
-
-                val isRightEdgeItem = right1 || right2
-
-                // Check if this item's distance to center item is smaller than half size of
-                // the indicator count when current indicator at the center or
-                // when we reach the end of list. End of the list only one item is on edge
-                // with 10 items and 7 indicators
-                // 7-3= 4th item can be the first valid left edge item and
-                val isLeftEdgeItem =
-                    index <= currentItem - centerItemIndex &&
-                            currentItem > centerItemIndex &&
-                            index < itemCount - indicatorCount + 1
-
-
-                val neighbor =
-                    (currentItem > 0 && index == currentItem - 1) ||
-                            (index == currentItem + 1 && currentItem < itemCount - 2)
-
-
-                println(
-                    "⚾️ LazyRow index: $index, right1: $right1, right2: $right2, " +
-                            "currentPageOffset: $currentPageOffset"
-                )
-
-//                text =
-//                    "index: $index, right1: $right1, right2: $right2, neighbor: $neighbor, " +
-//                            "currentPageOffset: $currentPageOffset"
-
-                Box(
-                    modifier = Modifier
-                        .graphicsLayer {
-
-//                                val scale = 1f
-                            val scale = if (isSelected) {
-                                1f
-                            } else if (isLeftEdgeItem || isRightEdgeItem) {
-                                .5f
-                            } else {
-                                .8f
-                            }
-                            scaleX = scale
-                            scaleY = scale
-
-                        }
-
-                        .clip(indicatorShape)
-                        .size(indicatorSize)
-                        .background(
-                            if (isSelected) activeColor else inActiveColor,
-                            indicatorShape
-                        )
-                        .clickable {
-                            coroutineScope.launch {
-                                onClick?.invoke(this, index)
-                            }
-                        }
-                )
-            }
+        if (orientation == IndicatorOrientation.Horizontal) {
+            listState.animateScrollToItem(
+                currentItem,
+                (widthInPx / 2 - viewportSize.width / 2).toInt()
+            )
+        } else {
+            listState.animateScrollToItem(
+                currentItem,
+                (widthInPx / 2 - viewportSize.height / 2).toInt()
+            )
         }
     }
 
-    Text(text)
+    if (orientation == IndicatorOrientation.Horizontal) {
+        LazyRow(
+            modifier = modifier.width(totalWidth),
+            state = listState,
+            contentPadding = PaddingValues(vertical = space),
+            horizontalArrangement = Arrangement.spacedBy(space),
+            userScrollEnabled = false
+        ) {
+            indicatorItems(
+                itemCount,
+                currentItem,
+                indicatorCount,
+                indicatorShape,
+                activeColor,
+                inActiveColor,
+                indicatorSize,
+                onClick
+            )
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier.height(totalWidth),
+            state = listState,
+            contentPadding = PaddingValues(horizontal = space),
+            verticalArrangement = Arrangement.spacedBy(space),
+            userScrollEnabled = false
+        ) {
+            indicatorItems(
+                itemCount,
+                currentItem,
+                indicatorCount,
+                indicatorShape,
+                activeColor,
+                inActiveColor,
+                indicatorSize,
+                onClick
+            )
+        }
+    }
+
 }
 
-private fun getScale(index: Int, currentPage: Int, indicatorCount: Int, itemCount: Int): Float {
-    return 1f
+private fun LazyListScope.indicatorItems(
+    itemCount: Int,
+    currentItem: Int,
+    indicatorCount: Int,
+    indicatorShape: Shape,
+    activeColor: Color,
+    inActiveColor: Color,
+    indicatorSize: Dp,
+    onClick: ((Int) -> Unit)?
+) {
+    items(itemCount) { index ->
+
+        val isSelected = (index == currentItem)
+
+        // Index of item in center when odd number of indicators are set
+        // for 5 indicators this is 2nd indicator place
+        val centerItemIndex = indicatorCount / 2
+
+        val right1 =
+            (currentItem < centerItemIndex &&
+                    index >= indicatorCount - 1)
+
+        val right2 =
+            (currentItem >= centerItemIndex &&
+                    index >= currentItem + centerItemIndex &&
+                    index < itemCount - centerItemIndex + 1)
+        val isRightEdgeItem = right1 || right2
+
+        // Check if this item's distance to center item is smaller than half size of
+        // the indicator count when current indicator at the center or
+        // when we reach the end of list. End of the list only one item is on edge
+        // with 10 items and 7 indicators
+        // 7-3= 4th item can be the first valid left edge item and
+        val isLeftEdgeItem =
+            index <= currentItem - centerItemIndex &&
+                    currentItem > centerItemIndex &&
+                    index < itemCount - indicatorCount + 1
+
+        Box(
+            modifier = Modifier
+                .graphicsLayer {
+                    val scale = if (isSelected) {
+                        1f
+                    } else if (isLeftEdgeItem || isRightEdgeItem) {
+                        .5f
+                    } else {
+                        .8f
+                    }
+                    scaleX = scale
+                    scaleY = scale
+
+                }
+
+                .clip(indicatorShape)
+                .size(indicatorSize)
+                .background(
+                    if (isSelected) activeColor else inActiveColor,
+                    indicatorShape
+                )
+                .then(
+                    if (onClick != null) {
+                        Modifier
+                            .clickable {
+                                onClick.invoke(index)
+                            }
+                    } else Modifier
+                )
+        )
+    }
+}
+
+enum class IndicatorOrientation {
+    Horizontal, Vertical
 }
